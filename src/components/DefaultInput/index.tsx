78286f74c4733ff481d4ef6a2a130f648e20eead
Pablo Daniel <pdaniel0.009@gmail.com>
2025-11-20 18:28:05 -0300

    Configurando componente input

---
+++ src/components/DefaultInput/index.tsx
@@ -0,0 +1,10 @@
+type DefaultInputProps = {id: string} & React.ComponentProps<'input'>;
+
+export function DefaultInput({ id, type }: DefaultInputProps) {
+    return (
+        <>
+            <label htmlFor='meuInput'>task</label>
+            <input id={id} type={type} />
+        </>
+    )
+}
